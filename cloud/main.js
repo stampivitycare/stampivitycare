d128c45b3862e8746c8043ab612947fa09d93b40
stampivitycare <stampivitycare@gmail.com>
2017-03-01 11:14:32 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -9,12 +9,12 @@ Parse.Cloud.define("sendSMS", function(request, response) {
   // Require and initialize the Twilio module with your credentials
          
 //var client = require('twilio')('AC10b0b6b7218e1da28313e3bff9d51bba','6fb83314fdfe3a24b947df1a7754ace5');
-var client = require('twilio')('ACc0f4894c7ae5d90bdad29fa0d09b3e2f','965bf4332306ccb0131eb19d2b3a5f82');
+var client = require('twilio')('AC3ce509f4e270567a423b8980d6121032','9435ac4a1aff13d6851fbd04f70cbe12');
           
            
 // Send an SMS message
 client.sendSms({
-    to: '+918977875774',
+    to: '+919951121245',
     //from: '+16467627663', 
     from: '+919975-5360',
     body: 'hai'
